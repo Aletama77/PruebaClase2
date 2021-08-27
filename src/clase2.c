d5f41eac15229cb31d088e46ce032a959bf13b1f
Aletama77 <alexandertamashiro@gmail.com>
2021-08-26 21:42:20 -0300

    primer commit

---
+++ src/clase2.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : clase2.c
+ Author      : aletama77
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+
+
+	return EXIT_SUCCESS;
+}
